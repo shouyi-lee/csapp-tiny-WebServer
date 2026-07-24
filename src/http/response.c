abe54cf1d812cf0fbc04a7a4ec743e5d47ce762f
LY <set.tsu.an@qq.com>
2026-07-24 18:48:49 +0800

    解决响应头无响应体长度问题

--- src/http/response.c
+++ src/http/response.c
@@ -43,6 +43,7 @@ ssize_t clienterror(rio_t *rp, const char *code)
     if (!strcmp("501", code))
         if (send_responsehead(rp, "Connection", "close") < 0)
             return -1;
+    if (send_responsehead(rp, "Content-length", "0") < 0) return -1;
     if (send_responsehead(rp, NULL, NULL) < 0) return -1;
     return 0;
 }
